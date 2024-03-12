4b856db17733cb0758bcb3972d86df14452ac1f3
Taveion-P <tpatterson@mines.edu>
2024-03-12 09:39:34 -0600

    added stuff

---
+++ SDR_files/test_io/test_thing.py
@@ -0,0 +1,8 @@
+import numpy as np
+from scipy import fft
+
+qpsk = np.fromfile(open("C:/Users/tavei/OneDrive/Documents/GitHub/dsp/SDR_files/test_io/raw_qpsk"),dtype=np.complex64)
+
+
+bpsk = np.fromfile(open("C:/Users/tavei/OneDrive/Documents/GitHub/dsp/SDR_files/test_io/raw_qpsk",dtype=np.complex64))
+
