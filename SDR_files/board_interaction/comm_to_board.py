b56664436fcd6a96edb1ac8870adeecb3b05713b
Nathan Wasniak <njwasniak@mines.edu>
2024-04-08 18:52:17 -0600

    Added socket stuff for communication with the board.

--- SDR_files/board_interaction/comm_to_board.py
+++ SDR_files/board_interaction/comm_to_board.py
@@ -11,37 +11,41 @@ import random
 from gnuradio import gr
 
 class blk(gr.sync_block):  # other base classes are basic_block, decim_block, interp_block
-    """Embedded Python Block example - a simple multiply const"""
+    """Embedded Python Block - Randomly grab 128 samples and save to file or pass onto next block."""
 
-    def __init__(self, save_file="calla.np"):  # only default arguments here
+    def __init__(self, save_file="calla.np", num_points=128):  # only default arguments here
         """arguments to this function show up as parameters in GRC"""
         gr.sync_block.__init__(
             self,
             name='Embedded Python Block',   # will show up in GRC
             in_sig=[np.complex64],
-            out_sig=None
-            # out_sig=[np.complex64]
+            # out_sig=None,
+            out_sig=[np.complex64]
         )
         # if an attribute with the same name as a parameter is found,
         # a callback is registered (properties work, too).
         self.save_file = save_file 
+        self.num_points = num_points
 
     def work(self, input_items, output_items):
         """example: multiply with constant"""
-        # output_items[0][:] = input_items[0] * self.example_param
+        output_items[0] = np.array([0], dtype=np.complex64)
+
+
 
         mystery_num = random.randint(1,1_000)
         if(mystery_num == 1):
             # print("Data ,", input_items[0])
             # print(type(input_items))
             # print(len(input_items))
-            if (len(input_items[0]) >= 128):
-                start = random.randint(0, len(input_items[0])-128)
-                data = input_items[0][start:start+128]
-                # output_items[0] = data
-                print(data)
-                data.tofile(self.save_file)
-                # np.save(self.save_file, data, allow_pickle=True)
+            if (len(input_items[0]) >= self.num_points):
+                start = random.randint(0, len(input_items[0])-self.num_points)
+                data = input_items[0][start:start+self.num_points]
+                output_items[0] = data
+                # print(data)
+                print("Collected {} samples".format(len(data)))
+                # data.tofile(self.save_file)
+                np.save(self.save_file, data, allow_pickle=True)
                 
         return len(input_items[0])
         # return (len(output_items[0]))
